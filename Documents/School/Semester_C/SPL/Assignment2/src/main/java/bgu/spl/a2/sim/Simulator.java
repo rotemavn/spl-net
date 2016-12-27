40ff872ddc8c239fe3b082d8a49d7984ede2a298
rotemavn <rotemavn@post.bgu.ac.il>
2016-12-27 16:27:46 +0200

    winished simulator

--- Documents/School/Semester_C/SPL/Assignment2/src/main/java/bgu/spl/a2/sim/Simulator.java
+++ Documents/School/Semester_C/SPL/Assignment2/src/main/java/bgu/spl/a2/sim/Simulator.java
@@ -6,10 +6,16 @@
 package bgu.spl.a2.sim;
 
 import bgu.spl.a2.WorkStealingThreadPool;
+import bgu.spl.a2.sim.conf.ManufactoringPlan;
+import bgu.spl.a2.sim.tasks.manufactoringTask;
+import bgu.spl.a2.sim.tools.*;
 import com.google.gson.*;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Vector;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
 
 
 /**
@@ -18,12 +24,45 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class Simulator {
 
     private static WorkStealingThreadPool pool;
+    private final static Warehouse warehouse=new Warehouse();
+    private final static ConcurrentLinkedQueue<Vector<manufactoringTask>> waves=new ConcurrentLinkedQueue();
+    private static ConcurrentLinkedQueue<Product> results;
 
     /**
      * Begin the simulation
      * Should not be called before attachWorkStealingThreadPool()
      */
-    public static ConcurrentLinkedQueue<Product> start(){return null;}
+    public static ConcurrentLinkedQueue<Product> start() {
+        while(pool==null); //wait until pool is not null
+        results=new ConcurrentLinkedQueue<>();
+
+        while(!waves.isEmpty()){
+            try {
+                Vector<manufactoringTask> currentWave = waves.peek();
+                CountDownLatch latch=new CountDownLatch(currentWave.size());
+                for (manufactoringTask task: currentWave) {
+                    pool.submit(task);
+                    task.getResult().whenResolved(() -> {
+                        latch.countDown();
+                    });
+                }
+                latch.await();
+
+                while (latch.getCount()>0);
+                waves.poll();
+
+            }
+            catch (NullPointerException e){
+
+            } catch (InterruptedException e) {
+
+            }
+
+        }
+
+
+        return results;
+    }
 
 
     /**
@@ -34,23 +73,79 @@ public class Simulator {
         pool=myWorkStealingThreadPool;
     }
 
-    //TODO
+    /**
+     * The function finds the requirements for the tools in the current run according to the JSON file
+     * @param toolsArray Json array given as input for tools
+     */
     private static void initTools(JsonArray toolsArray){
         for(int i=0; i<toolsArray.size(); i++){
+           JsonObject obj=(JsonObject)toolsArray.get(i);
+           String name=obj.get("tool").getAsString();
+           int qty=obj.get("qty").getAsInt();
+
+           Tool t;
+           if(name.equals("gs-driver")){
+               t=new GcdScrewDriver();
+           }
+           else if(name.equals("rs-pliers")){
+               t=new RandomSumPliers();
+           }
+
+           else{
+               t=new NextPrimeHammer();
+           }
+
+            warehouse.addTool(t,qty);
 
         }
     }
 
-    //TODO
+    /**
+     * The function finds the requirements for the tools in the current run according to the JSON file
+     * @param plansArray Json array given as input for tools
+     */
     private static void initPlans(JsonArray plansArray){
         for(int i=0; i<plansArray.size(); i++){
+            JsonObject obj=(JsonObject)plansArray.get(i);
+            String product=obj.get("product").getAsString();
 
+            JsonArray tools=obj.get("tools").getAsJsonArray();
+            String[] toolsToAdd=new String[tools.size()];
+            for(int j=0; j<tools.size(); j++){
+                JsonPrimitive toolObj=(JsonPrimitive)tools.get(j);
+                String toolName=toolObj.getAsString();
+                toolsToAdd[j]=toolName;
+            }
+
+
+            JsonArray parts=obj.get("parts").getAsJsonArray();
+            String[] partsToAdd=new String[parts.size()];
+            for(int j=0; j<parts.size(); j++){
+                JsonPrimitive partsObj=(JsonPrimitive)parts.get(j);
+                String partName=partsObj.getAsString();
+                partsToAdd[j]=partName;
+            }
+            warehouse.addPlan(new ManufactoringPlan(product,toolsToAdd,partsToAdd));
         }
     }
 
-    //TODO
+
     private static void initWaves(JsonArray wavesArray){
         for(int i=0; i<wavesArray.size(); i++){
+            JsonArray wave=(JsonArray)wavesArray.get(i);
+            for(int j=0; j<wave.size(); j++){
+                JsonObject obj=(JsonObject)wave.get(j);
+                String nameOfProduct=obj.get("product").getAsString();
+                ManufactoringPlan plan=warehouse.getPlan(nameOfProduct);
+                int qty=obj.get("qty").getAsInt();
+                long startID=obj.get("startId").getAsLong();
+
+                Vector<manufactoringTask> waveVector=new Vector<>();
+                for(int x=0; x<qty; x++){
+                    waveVector.add(new manufactoringTask(startID+x,warehouse,plan));
+                }
+                waves.add(waveVector);
+            }
 
         }
     }
@@ -83,7 +178,7 @@ public class Simulator {
 
             simulator.start();
 
-            //
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
--- Documents/School/Semester_C/SPL/Assignment2/src/main/java/bgu/spl/a2/sim/Warehouse.java
+++ Documents/School/Semester_C/SPL/Assignment2/src/main/java/bgu/spl/a2/sim/Warehouse.java
@@ -1,8 +1,12 @@
 package bgu.spl.a2.sim;
 
-import bgu.spl.a2.sim.tools.Tool;
+import bgu.spl.a2.sim.tools.*;
 import bgu.spl.a2.sim.conf.ManufactoringPlan;
 import bgu.spl.a2.Deferred;
+
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * A class representing the warehouse in your simulation
@@ -14,11 +18,27 @@ import bgu.spl.a2.Deferred;
  *
  */
 public class Warehouse {
-
+    private final Vector<String> indexMapping;
+    private final Vector<ConcurrentLinkedQueue> toolsLists;
+    private final ConcurrentHashMap<String,ManufactoringPlan> plans;
     /**
      * Constructor
      */
-    public Warehouse(){}
+    public Warehouse(){
+        indexMapping=new Vector<String>();
+        indexMapping.add(0,"GcdScrewDriver");
+        indexMapping.add(1,"NextPrimeHammer");
+        indexMapping.add(2,"RandomSumPliers");
+
+        toolsLists=new Vector<>();
+        toolsLists.add(0,new ConcurrentLinkedQueue<GcdScrewDriver>());
+        toolsLists.add(1,new ConcurrentLinkedQueue<NextPrimeHammer>());
+        toolsLists.add(2,new ConcurrentLinkedQueue<RandomSumPliers>());
+
+        plans=new ConcurrentHashMap<>();
+
+
+    }
 
     /**
      * Tool acquisition procedure
@@ -26,7 +46,9 @@ public class Warehouse {
      * @param type - string describing the required tool
      * @return a deferred promise for the  requested tool
      */
-    public Deferred<Tool> acquireTool(String type){return null;}
+    public Deferred<Tool> acquireTool(String type){
+        return null;
+    }
 
     /**
      * Tool return procedure - releases a tool which becomes available in the warehouse upon completion.
@@ -40,19 +62,70 @@ public class Warehouse {
      * @param product - a string with the product name for which a ManufactoringPlan is desired
      * @return A ManufactoringPlan for product
      */
-    public ManufactoringPlan getPlan(String product){return null;}
+    public ManufactoringPlan getPlan(String product){
+        return plans.get(product);
+    }
 
     /**
      * Store a ManufactoringPlan in the warehouse for later retrieval
      * @param plan - a ManufactoringPlan to be stored
      */
-    public void addPlan(ManufactoringPlan plan){}
+    public void addPlan(ManufactoringPlan plan){
+        String planName=plan.getProductName();
+        plans.put(planName,plan);
+    }
 
     /**
      * Store a qty Amount of tools of type tool in the warehouse for later retrieval
      * @param tool - type of tool to be stored
      * @param qty - amount of tools of type tool to be stored
      */
-    public void addTool(Tool tool, int qty){}
+    public void addTool(Tool tool, int qty){
+        String type=tool.getType();
+
+        if(type.equals("gs-driver"))
+            addGcdScrewDriver(qty);
+        else if(type.equals("np-hammer"))
+            addNextPrimeHammer(qty);
+        else if(type.equals("rs-pliers"))
+            addRandomSumPliers(qty);
+        else{
+            throw new IllegalArgumentException("The given tool is not a legal type of tool "+type);
+        }
+    }
+
+    /**
+     * The function initializes the list of GcdScrewDriver
+     * @param qty - number of tools to create
+     */
+    private void addGcdScrewDriver(int qty){
+        for(int i=0; i<qty; i++){
+            GcdScrewDriver tool=new GcdScrewDriver();
+            toolsLists.elementAt(0).add(tool);
+        }
+    }
+
+    /**
+     * The function initializes the list of NextPrimeHammer
+     * @param qty - number of tools to create
+     */
+    private void addNextPrimeHammer(int qty){
+        for(int i=0; i<qty; i++){
+            NextPrimeHammer tool=new NextPrimeHammer();
+            toolsLists.elementAt(1).add(tool);
+        }
+    }
+
+
+    /**
+     * The function initializes the list of RandomSumPliers
+     * @param qty - number of tools to create
+     */
+    private void addRandomSumPliers(int qty){
+        for(int i=0; i<qty; i++){
+            RandomSumPliers tool=new RandomSumPliers();
+            toolsLists.elementAt(2).add(tool);
+        }
+    }
 
 }
